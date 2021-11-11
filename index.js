23cfb20d29f7b653dd893277a5627f3245b4518f
Piyal <chakravertypiyal@gmail.com>
2021-11-11 23:49:06 +0530

    Comments added to JS file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// HTML elements initialized
 const scoresElem = [document.querySelector(".score1"), document.querySelector(".score2")];
 const currScoresElem = [document.querySelector(".curr-score1"), document.querySelector(".curr-score2")];
 const btnNewGame = document.querySelector(".btn-new-game");
@@ -8,13 +9,12 @@ const btnHold = document.querySelector(".btn-hold");
 const imgDice = document.querySelector(".img-dice");
 const secPlayers = [document.querySelector(".sec-player1"), document.querySelector(".sec-player2")];
 
+// Active player and scores initialized
 let activePlayer = 0;
 let scores = [0, 0];
 let currScores = [0, 0];
 
-secPlayers[0].classList.add("sec-highlight");
-secPlayers[1].classList.add("sec-other");
-
+// Update all scores when new game begins
 function newGame() {
     for (let i = 0; i < 2; i++) {
         currScoresElem[i].textContent = currScores[i];
@@ -23,18 +23,24 @@ function newGame() {
 }
 
 newGame();
+// Highlight the active player
+secPlayers[0].classList.add("sec-highlight");
+secPlayers[1].classList.add("sec-other");
 
+// Generate random number between 1 and 6, and show it on dice.
 function showRandomNumber() {
     const randomNum = Math.floor(Math.random() * 6) + 1;
     imgDice.src = `images/dice-${randomNum}.png`;
     return randomNum;
 }
 
+// Set current score of active player = 0
 function updateCurrScore() {
     currScores[activePlayer] = 0;
     currScoresElem[activePlayer].textContent = currScores[activePlayer];
 }
 
+// Change style when active player changes (Highlight active player)
 function changePlayer() {
     secPlayers[activePlayer].classList.remove("sec-highlight");
     secPlayers[activePlayer].classList.add("sec-other");
@@ -43,21 +49,26 @@ function changePlayer() {
     secPlayers[activePlayer].classList.add("sec-highlight");
 }
 
+// On clicking role dice button
 btnRoll.addEventListener("click", function () {
+    // Show dice image if hidden in the beginning
     if (imgDice.classList.contains("hidden")) {
         imgDice.classList.remove("hidden");
     }
     const activeDice = showRandomNumber();
+    // If dice value is 1, change player and lose current score
     if (activeDice === 1) {
         updateCurrScore();
         changePlayer();
     }
+    // Else, add current score and continue
     else {
         currScores[activePlayer] += activeDice;
         currScoresElem[activePlayer].textContent = currScores[activePlayer];
     }
 });
 
+// On clicking Hold button, update total score and change player
 btnHold.addEventListener("click", function () {
     scores[activePlayer] += currScores[activePlayer];
     scoresElem[activePlayer].textContent = scores[activePlayer];
@@ -65,6 +76,7 @@ btnHold.addEventListener("click", function () {
     changePlayer();
 });
 
+// On clicking New Game GamepadButton, reset everything
 btnNewGame.addEventListener("click", function () {
     if (activePlayer == 1) {
         changePlayer();
